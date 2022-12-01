470d7137b21a7b5370b8f2a19164ce81990b570d
Hector Cardenas <card.hectorl@gmail.com>
2022-12-01 17:58:58 -0600

    added footer component. added social icons. added css.

--- blc/src/App.js
+++ blc/src/App.js
@@ -3,12 +3,14 @@ import Navbar from './Components/Navbar';
 import Menu from './Components/Menu';
 import Section from './Components/Section';
 import {Routes, Route, Link} from 'react-router-dom'
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <>
   <Navbar/>
   <Section/>
+  <Footer/>
 
   <Routes>
     <Route path='/' ></Route>
