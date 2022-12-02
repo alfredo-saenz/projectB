e05ef1431c4fd43a5e83f70ec1c00e739035b9b0
alfredo-saenz <alfredosaenz104@gmail.com>
2022-12-01 18:56:32 -0600

    Deleted two functions from app.js

--- blc/src/App.js
+++ blc/src/App.js
@@ -2,32 +2,23 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import Menu from './Components/Menu';
 import Section from './Components/Section';
-import {Routes, Route, Link} from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom';
 import { useRef } from 'react';
 import Footer from './Components/Footer';
 
 function App() {
-  const ref = useRef(null);
+	return (
+		<>
+			<Navbar />
+			<Section />
+			<Footer />
 
-	const handleClick = () => {
-		ref.current?.scrollIntoView({ behavior: 'smooth' });
-	};
-
-  return (
-    <>
-  <Navbar handleClick={handleClick} ref={ref}/>
-  <Section/>
-  <Footer/>
-
-  <Routes>
-    <Route path='/' ></Route>
-    <Route path='/menu' element={<Menu/>}></Route>
-  </Routes>
-    </>
-
-
-  );
+			<Routes>
+				<Route path='/'></Route>
+				<Route path='/menu' element={<Menu />}></Route>
+			</Routes>
+		</>
+	);
 }
-
 
 export default App;
