c45f9e12c89d8576df61354830f59e50c0727d19
Hector Cardenas <card.hectorl@gmail.com>
2022-12-05 22:50:25 -0600

    added menu page created from menu item objs, added useState for full nav to change tranform style onClick, removed links from section menu btn and added useNavigate()

--- blc/src/Components/Menu.js
+++ blc/src/Components/Menu.js
@@ -1,39 +1,36 @@
 import React from 'react';
 import { createMenuObj } from './createMenuObj.js'
-import Footer from './Footer';
-import  NavBar  from "./Navbar";
-
+import  "./Menu.css";
+import burritoImg from "../Images/mexican-g6e202bec1_640.jpg";
+import tacoImg from "../Images/tacos-gc2071a96a_640.jpg";
+import tacoPLatterImg from "../Images/tortilla-g9b87caffa_640.jpg";
+import chipsImg from '../Images/food-gb7760b7fb_640.jpg';
 function Menu(props) {
 
-    const tacoItem = createMenuObj('../Images/tacos-gc2071a96a_640.jpg','Tacos','Fresh meat tacos from culican', 2.50);
-    const burritoItem = createMenuObj('../Images/mexican-g6e202bec1_640.jpg','Burrito','fully loaded burrito sinaloa style.', 4.50);
-    const chipsItem = createMenuObj('../Images/food-gb7760b7fb_640.jpg','Chips','Fresh tortilla chips stolen from chipotle', 2.50);
-    const taco2Item = createMenuObj('../Images/tortilla-g9b87caffa_640.jpg','Tacos','Yesterdays meat tacos from culican', 1.50);
+    // Menu items are created with createMenuObj() factory function.
+    //This can be used in the future if admin page is created so user can create their own menu items automatically.
+    const tacoItem = createMenuObj(1,tacoImg,'Tacos','Fresh meat tacos from culican', 2.50);
+    const burritoItem = createMenuObj(2,burritoImg,'Burrito','fully loaded burrito sinaloa style.', 4.50);
+    const chipsItem = createMenuObj(3,chipsImg,'Chips','Fresh tortilla chips stolen from chipotle', 2.50);
+    const taco2Item = createMenuObj(4,tacoPLatterImg,'Tacos','Yesterdays meat tacos from culican', 1.50);
 
-    const menuItemArray = [tacoItem,burritoItem,chipsItem,taco2Item];
+    const menuItems= [tacoItem,burritoItem,chipsItem,taco2Item];
 
-    const menuItems = [];
-    for (let i = 0; i < menuItemArray.length; i++) {
-        const imgSrc = menuItemArray[i][0];
-        const name = menuItemArray[i][1];
-        const description = menuItemArray[i][2];
-        const price = menuItemArray[i][3];
+    const listItems = menuItems.map(menuItems => 
+            <li key={menuItems.id} className="menuItems">
+                <img src={menuItems.img} alt="" className='menuImgs'/>
+                <h2>{menuItems.name}</h2>
+                <p>{menuItems.description}</p>
+                <p>{menuItems.price}</p>
+
+            </li>
         
-        menuItems.push(<div>
-
-            <img src={imgSrc} alt="" />
-            <h2>{name}</h2>
-            <p>{description}</p>
-            <p>{price}</p>
-        </div>)
-
-    }
+        ); 
 
     return (
         <>
-        <h2>Menu!</h2>       
-            {/* <div className='menu-container'>{ menuItems }</div> */}
-
+        <h2>Menu!</h2>
+        <ul>{listItems}</ul>
         </>
     );
 }
--- blc/src/Components/Section.js
+++ blc/src/Components/Section.js
@@ -1,35 +1,42 @@
-import React, { Component } from 'react';
-import outsideView from '../Images/outside-view.jpg';
-import './Section.css';
-import {Link} from 'react-router-dom';
+import React, { Component } from "react";
+import outsideView from "../Images/outside-view.jpg";
+import "./Section.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export default class Section extends Component {
+function Section(props) {
+  
+	const navigate = useNavigate();
 
+	return (
+		<>
+			<div className="section-container">
+				<img src={outsideView} alt="" className="section-img" />
 
-	render() {
-		return (
-			<>
+				<h1 className="section-h1">
+					<q>
+						Great food, excellent service. Burritos are fat full of goodness.
+					</q>
+					-John C.
+				</h1>
 
-        <div className='section-container'>
-           
-            <img src={outsideView} alt="" className='section-img'/>
-            
-            <h1 className='section-h1'><q>Great food, excellent service. Burritos are fat full of goodness.</q>-John C.</h1>
+				<button onClick={() => navigate("/menu")} className="menu-btn">
+					MENU
+				</button>
 
-            
-            <button className='menu-btn'><Link to='/menu'>MENU </Link></button>
-           
-        </div>
-				<div id='about' className='about-us'>
-          <h2 className='about-us-h2'>About Us</h2>
-          <p className='about-us-p'>Established in 2008, our goal was to have the best Mexican food in Kansas City.</p>
-
-        </div>
-        <div id='location' className='location-section'>
-          <h2>Come Visit Us</h2>
-          
-        </div>
-			</>
-		);
-	}
+			</div>
+			<div id="about" className="about-us">
+				<h2 className="about-us-h2">About Us</h2>
+				<p className="about-us-p">
+					Established in 2008, our goal was to have the best Mexican food in
+					Kansas City.
+				</p>
+			</div>
+			<div id="location" className="location-section">
+				<h2>Come Visit Us</h2>
+			</div>
+		</>
+	);
 }
+
+export default Section;
