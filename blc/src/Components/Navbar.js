42040c3949fc27b13f36d0844202c7e93e6e4cc8
alfredo-saenz <alfredosaenz104@gmail.com>
2022-12-22 17:05:54 -0600

    Fully responsive navbar

--- blc/src/Components/Navbar.js
+++ blc/src/Components/Navbar.js
@@ -1,94 +1,65 @@
-import React from "react";
+import React from 'react';
 // import { Link } from 'react-router-dom';
-import logo from "../Images/logo.png";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { useRef } from "react";
-import "./Navbar.css";
-import { Link } from "react-scroll";
-import { Link as LinkToComp } from "react-router-dom";
-import { useState } from "react";
+import logo from '../Images/logo.png';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useRef } from 'react';
+import './Navbar.css';
+import { Link } from 'react-scroll';
+import { Link as LinkToComp } from 'react-router-dom';
+import { useState } from 'react';
 
 function Navbar(props) {
-	const [isVisible, setVisibility] = useState(false);
-
-	//this function is used to change between two different styles.
-	const handleClick = () => {
-		setVisibility((current) => !current);
-	};
-
 	const navRef = useRef();
 
+	const showNavbar = () => {
+		navRef.current.classList.toggle('responsive_nav');
+	};
+
 	return (
-		<>
-			<nav
-				// this changes the transform style when the hamburger btn is clicked... hamburger btn has a onClick() event.-hc
-				style={{
-					transform: isVisible ? "none" : "",
-				}}
-				className="full-nav"
-				ref={navRef}
-			>
-				<div className="exit-nav-btn">
-					<button className="nav-btn" onClick={handleClick}>
-						<FaTimes />
-					</button>
-				</div>
+		<header>
+			<div className='home-logo navbar-title navbar-item'>
 
-				<LinkToComp className="nav-logo-container" to="/">
-					<img
-						className="nav-logo"
-						src={logo}
-						alt="Burritos la chiquita Logo"
-						onClick={handleClick}
-					/>
-				</LinkToComp>
-				<ul
-					className="nav-items-container"
-					style={{ listStyle: "none", cursor: "pointer" }}
-				>
-					<LinkToComp onClick={handleClick} to="/menu">
-						<li>Menu</li>
-					</LinkToComp>
-					<LinkToComp onClick={handleClick} to="/gallery">
-						<li>Gallery</li>
-					</LinkToComp>
-
-					<li>
-						<Link
-							activeClass="active"
-							to="about"
-							spy={true}
-							smooth={true}
-							//this will close the full-nav after a section is clicked.
-							onClick={handleClick}
-						>
-							About Us
-						</Link>
-					</li>
-					<li>
-						<Link
-							activeClass="active"
-							to="location"
-							spy={true}
-							smooth={true}
-							//this will work when location section is added.
-							onClick={handleClick}
-						>
-							Location
-						</Link>
-					</li>
-				</ul>
-			</nav>
-
-			<div className="nav-container">
-				<LinkToComp to="/">
-					<img src={logo} alt="" srcset="" className="nav-logo" />
-				</LinkToComp>
-				<button className="nav-btn" onClick={handleClick}>
-					<FaBars />
-				</button>
+			<LinkToComp to='/'>
+			<img className='home-logo' src={logo}/>
+			</LinkToComp>
 			</div>
-		</>
+
+			<nav className='nav-section' ref={navRef}>
+				<a className='navbar-item'>
+					<LinkToComp to='/menu' onClick={showNavbar}>Menu</LinkToComp>
+				</a>
+				<a className='navbar-item'>
+					<LinkToComp to='/gallery'>Gallery</LinkToComp>
+				</a>
+				<a className='navbar-item'>
+					<Link
+						activeClass='active'
+						to='about'
+						spy={true}
+						smooth={true}
+						onClick={showNavbar}>
+						About Us
+					</Link>
+				</a>
+				<a className='navbar-item'>
+					<Link
+						activeClass='active'
+						to='location'
+						spy={true}
+						smooth={true}
+						onClick={showNavbar}>
+						Location
+					</Link>
+				</a>
+
+				<button className='nav-btn nav-close-btn' onClick={showNavbar}>
+					<FaTimes />
+				</button>
+			</nav>
+			<button className='nav-btn' onClick={showNavbar}>
+				<FaBars />
+			</button>
+		</header>
 	);
 }
 
