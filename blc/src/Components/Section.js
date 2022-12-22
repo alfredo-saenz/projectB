f82d54c7273e3a55ce3e5d56c3ee109407c92bf4
cardhect <card.hectorl@gmail.com>
2022-12-22 17:09:46 -0600

    Main section changed with new image and new text overlay

--- blc/src/Components/Section.js
+++ blc/src/Components/Section.js
@@ -1,41 +1,39 @@
 import React, { Component } from "react";
-import outsideView from "../Images/outside-view.jpg";
+import blcEnhanced from "../Images/BurritoLaChiq-enhanced.jpeg";
+import mexicoChef from "../Images/mexican-chef.jpeg";
 import "./Section.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import Location from "./Location";
 
 function Section(props) {
-
 	const navigate = useNavigate();
 
 	return (
 		<>
 			<div className="section-container">
-				<img src={outsideView} alt="" className="section-img" />
-
-				<h1 className="section-h1">
-					<q>
-						Great food, excellent service. Burritos are fat full of goodness.
-					</q>
-					-John C.
-				</h1>
-
-				<button onClick={() => navigate("/menu")} className="menu-btn">
-					MENU
-				</button>
-
+				<img src={blcEnhanced} alt="" className="section-img" />
+				<div className="image-overlay"></div>
+				<div className="header-menu-container">
+					<h1 className="section-h1">
+						Authentic Mexican cuisine served in a lively atmosphere.<br></br>
+						Come taste the flavors of Mexico at our restaurant!
+					</h1>
+					<button onClick={() => navigate("/menu")} className="menu-btn">
+						MENU
+					</button>
+				</div>
 			</div>
 			<div id="about" className="about-us">
 				<h2 className="about-us-h2">About Us</h2>
+				<img className="aboutus-img" src={mexicoChef} alt="" />
 				<p className="about-us-p">
-					Established in 2008, our goal was to have the best Mexican food in
-					Kansas City.
+				Burritos La Chiquita has a rich history and a passion for authentic Mexican cuisine.<br></br>Our warm and welcoming atmosphere is the perfect place to gather with friends and family and enjoy a delicious meal.<br></br> Thank you for choosing to dine with us. We can't wait to serve you!
 				</p>
 			</div>
 			<div id="location" className="location-section">
 				<h2>Come Visit Us</h2>
-          <Location/>
+				<Location />
 			</div>
 		</>
 	);
